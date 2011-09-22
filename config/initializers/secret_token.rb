eb5ca061e3d58781ab78a2eac2bea7c2f3bba1a3
U-LUXRESEARCHINC\chris.greene <chris.greene@lux-bos-e6320-6.luxresearchinc.com>
2011-09-22 10:07:16 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NsCatalog::Application.config.secret_token = 'cd9f574ae69d12fa6efeddd531eecb38fa34b09bcd4b6966eae2c7189fae8bd9454b768c49d63612ebb81602f3b784336b9d57bf4971c767536c94f5165c5225'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NsCatalog::Application.config.session_store :cookie_store, :key => '_ns_catalog_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NsCatalog::Application.config.session_store :active_record_store
